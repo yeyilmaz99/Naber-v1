102ed48e6e5f18ef88030e1327b7f24d1c26d0ba
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-27 01:20:45 +0300

    bugFixesV1

--- src/assets/js/base.js
+++ src/assets/js/base.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     setTimeout(() => {
         $("#loader").fadeToggle(250);
     }, 700); // hide delay when page load
+    console.log("ready");
 });
 ///////////////////////////////////////////////////////////////////////////
 
